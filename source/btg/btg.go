a688f4f69b6b827c775cbc1e8a4171fd3f9de64f
Jean Gonçalves <jean.gnc@gmail.com>
2024-05-18 12:18:31 -0300

    Extracts basic transaction info

--- source/btg/btg.go
+++ source/btg/btg.go
@@ -1,11 +1,37 @@
 package btg
 
 import (
+	"regexp"
+
 	"github.com/jeangnc/financial-agent/pdf"
 )
 
+const DATE_REGEXP = `(?<date>[0-9]{2} [a-zA-Z]{3})`
+const AMOUNT_REGEXP = `(?<amount>(?:- ?)?[A-Z]{1,}\$ (?:[0-9]+\.?)+\,[0-9]{2})`
+const DESCRIPTION_REGEXP = `(?<description>([^R]|R[^$])*)`
+const TRANSACTION_REGEXP = `(` + DATE_REGEXP + `\W+` + DESCRIPTION_REGEXP + `\W+` + AMOUNT_REGEXP + `)`
+
 type T map[string]string
 
-func ParseFile(p pdf.File) []T {
-	return []T{}
+func ParseFile(f pdf.File) []T {
+	var result = make([]T, 0)
+	var expr = regexp.MustCompile(TRANSACTION_REGEXP)
+
+	for _, p := range f.Pages {
+		matches := expr.FindAllStringSubmatch(p.Content, -1)
+
+		for _, m := range matches {
+			t := T{}
+
+			for i, name := range expr.SubexpNames() {
+				if name != "" {
+					t[name] = m[i]
+				}
+			}
+
+			result = append(result, t)
+		}
+	}
+
+	return result
 }
