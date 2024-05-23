0f46fee3124171a7a49d3125f119387da7e5b88d
Jean Gonçalves <jean.gnc@gmail.com>
2024-05-23 19:22:51 -0300

    Moves regexp logic to a separate package

---
+++ regexp/regexp.go
@@ -0,0 +1,46 @@
+package regexp
+
+import (
+	"fmt"
+	"regexp"
+)
+
+type RegexpMatch map[string]string
+
+func Remove(pattern string, s string) string {
+	expr := regexp.MustCompile(pattern)
+	return expr.ReplaceAllString(s, "")
+}
+
+func Match(pattern string, text string) (RegexpMatch, error) {
+	matches := MatchAll(pattern, text)
+
+	if len(matches) == 0 {
+		return nil, nil
+	}
+
+	if len(matches) > 1 {
+		return nil, fmt.Errorf("multiple matches for the pattern: %s", pattern)
+	}
+
+	return matches[0], nil
+}
+
+func MatchAll(pattern string, text string) []RegexpMatch {
+	result := make([]RegexpMatch, 0)
+	expr := regexp.MustCompile(pattern)
+
+	for _, m := range expr.FindAllStringSubmatch(text, -1) {
+		t := RegexpMatch{}
+
+		for i, name := range expr.SubexpNames() {
+			if name != "" {
+				t[name] = m[i]
+			}
+		}
+
+		result = append(result, t)
+	}
+
+	return result
+}
--- source/btg/btg.go
+++ source/btg/btg.go
@@ -2,12 +2,12 @@ package btg
 
 import (
 	"fmt"
-	"regexp"
 	"strconv"
 	"strings"
 	"time"
 
 	"github.com/jeangnc/financial-agent/pdf"
+	"github.com/jeangnc/financial-agent/regexp"
 	"github.com/jeangnc/financial-agent/types"
 )
 
@@ -17,14 +17,11 @@ const DESCRIPTION_REGEXP = `(?<description>([^R]|R[^$])*)`
 const TRANSACTION_REGEXP = `(` + DATE_REGEXP + `\W+` + DESCRIPTION_REGEXP + `\W+` + AMOUNT_REGEXP + `)`
 const INSTALLMENT_REGEXP = `\((?<current>\d+)\/(?<total>\d+)\)$`
 
-type RegexpMatch map[string]string
-
 func ParseFile(f pdf.File) ([]types.Transaction, error) {
 	var result = make([]types.Transaction, 0)
 
 	for _, p := range f.Pages {
-		expr := regexp.MustCompile(TRANSACTION_REGEXP)
-		for _, m := range matchAll(expr, p.Content) {
+		for _, m := range regexp.MatchAll(TRANSACTION_REGEXP, p.Content) {
 			amount, err := parseCurrency(m["amount"])
 			if err != nil {
 				return nil, fmt.Errorf("failed to convert amount: %s", err)
@@ -38,14 +35,13 @@ func ParseFile(f pdf.File) ([]types.Transaction, error) {
 				TotalInstallments:  1,
 			}
 
-			expr = regexp.MustCompile(INSTALLMENT_REGEXP)
-			m2, _ := match(expr, t.Description)
+			m2, _ := regexp.Match(INSTALLMENT_REGEXP, t.Description)
 
 			if m2 != nil {
 				currentInstallment, _ := strconv.ParseInt(m2["current"], 10, 64)
 				totalInstallments, _ := strconv.ParseInt(m2["total"], 10, 64)
 
-				t.Description = strings.TrimSpace(expr.ReplaceAllString(t.Description, ""))
+				t.Description = strings.TrimSpace(regexp.Remove(INSTALLMENT_REGEXP, t.Description))
 				t.CurrentInstallment = currentInstallment
 				t.TotalInstallments = totalInstallments
 			}
@@ -62,36 +58,4 @@ func parseCurrency(amountStr string) (float64, error) {
 	amountStr = strings.ReplaceAll(amountStr, ",", ".")
 	amountStr = strings.TrimSpace(amountStr)
 	return strconv.ParseFloat(amountStr, 64)
-}
-
-func match(expr *regexp.Regexp, text string) (RegexpMatch, error) {
-	matches := matchAll(expr, text)
-
-	if len(matches) == 0 {
-		return nil, nil
-	}
-
-	if len(matches) > 1 {
-		return nil, fmt.Errorf("multiple matches for the pattern: %s", expr)
-	}
-
-	return matches[0], nil
-}
-
-func matchAll(expr *regexp.Regexp, text string) []RegexpMatch {
-	result := make([]RegexpMatch, 0)
-
-	for _, m := range expr.FindAllStringSubmatch(text, -1) {
-		t := RegexpMatch{}
-
-		for i, name := range expr.SubexpNames() {
-			if name != "" {
-				t[name] = m[i]
-			}
-		}
-
-		result = append(result, t)
-	}
-
-	return result
 }
