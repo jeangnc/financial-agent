b148941fd5ad9470e17ed9662d832050b2bccfe5
Jean Gonçalves <jean.gnc@gmail.com>
2024-05-26 18:37:33 -0300

    Implements transaction creation

--- organizze/client.go
+++ organizze/client.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"time"
 )
 
 const BaseURL = "https://api.organizze.com.br/rest/v2"
@@ -25,6 +26,28 @@ type (
 		ParentID int64  `json:"parent_id"`
 		Name     string `json:"name"`
 	}
+
+	Tag struct {
+		Name string `json:"name"`
+	}
+
+	InstallmentAttributes struct {
+		Periodicity string  `json:"periodicity"`
+		Total       float64 `json:"total"`
+	}
+
+	Transaction struct {
+		Id                    int64                  `json:"id",omitempty`
+		CreditCardId          int64                  `json:"credit_card_id"`
+		CreditCardInvoiceId   int64                  `json:"credit_card_invoice_id"`
+		Description           string                 `json:"description"`
+		Notes                 string                 `json:"notes"`
+		Date                  time.Time              `json:"date"`
+		AmountCents           float64                `json:"amount_cents"`
+		CategoryId            int64                  `json:"category_id"`
+		InstallmentAttributes *InstallmentAttributes `json:"installments_attributes"`
+		Tags                  []Tag                  `json:"tags"`
+	}
 )
 
 func NewClient(name string, email string, apiKey string) *Client {
@@ -37,7 +60,7 @@ func NewClient(name string, email string, apiKey string) *Client {
 }
 
 func (c Client) Categories() (r []Category, err error) {
-	res, err := c.request(http.MethodGet, "categories", nil)
+	res, err := c.request(http.MethodGet, "categories", nil, nil)
 	if err != nil {
 		panic(err)
 	}
@@ -45,9 +68,22 @@ func (c Client) Categories() (r []Category, err error) {
 	return parseResponse[[]Category](res)
 }
 
-func (c Client) request(method, endpoint string, params map[string]string) (*http.Response, error) {
-	buf := new(bytes.Buffer)
-	json.NewEncoder(buf).Encode(params)
+func (c Client) CreateTransaction(t Transaction) error {
+	b, err := json.Marshal(t)
+	if err != nil {
+		return err
+	}
+
+	_, err = c.request(http.MethodPost, "transactions", nil, b)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c Client) request(method, endpoint string, params map[string]string, body []byte) (*http.Response, error) {
+	buf := bytes.NewBuffer(body)
 
 	url := fmt.Sprintf("%s/%s", BaseURL, endpoint)
 	req, err := http.NewRequest(method, url, buf)
@@ -55,13 +91,11 @@ func (c Client) request(method, endpoint string, params map[string]string) (*htt
 		return nil, err
 	}
 
-	/*
-		q := req.URL.Query()
-		for key, val := range params {
-			q.Set(key, val)
-		}
-		req.URL.RawQuery = q.Encode()
-	*/
+	q := req.URL.Query()
+	for key, val := range params {
+		q.Set(key, val)
+	}
+	req.URL.RawQuery = q.Encode()
 
 	authorization := "Basic " + base64.StdEncoding.EncodeToString([]byte(c.email+":"+c.apiKey))
 	req.Header.Add("Authorization", authorization)
